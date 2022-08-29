e887fbe08f7dcdd2044a28dbe34c90cbd2fbf9e4
matheusbail <101137680+matheusbail@users.noreply.github.com>
2022-08-29 13:49:47 -0300

    Add files via upload

---
+++ gift-drop/src/source.js
@@ -0,0 +1,11 @@
+let logo = document.querySelector("#logosite");
+
+logo.addEventListener('click', function(){
+    alert("Faça Login para acessar a loja")
+})
+
+let login = document.querySelector("#logar");
+login.addEventListener('click', function(){
+    alert("Funcionou")
+})
+   
